c8382001241c7f22fa47539bac51b3941b689f8a
Jaime Cabrera Flores <129076194+FRiJ0LiTO@users.noreply.github.com>
2025-05-25 23:13:45 -0600

    refactor Constant structure to use Value field and improve constant handling

--- internal/symbol/constants.go
+++ internal/symbol/constants.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Constant struct {
-	virtualDirection int
-	dataType         memory.DataType
+	Value    string
+	DataType memory.DataType
 }
 
 // AddConstant adds a new constant to the function directory.
@@ -23,18 +23,19 @@ type Constant struct {
 //   - If there is no active scope (i.e., CurrentScope.Height is 0), an error is returned.
 //   - If the constant already exists in the Constants map, the function does nothing and returns nil.
 //   - Otherwise, the constant is added to the Constants map with the provided virtual address.
-func (fd *FunctionDirectory) AddConstant(constant string, constantType memory.DataType, virtualAddress int) error {
+func (fd *FunctionDirectory) AddConstant(constantValue string, constantType memory.DataType, virtualAddress int) error {
 	if fd.CurrentScope.Height == 0 {
 		return fmt.Errorf("error: no active scope defined")
 	}
 
-	if _, exists := fd.Constants[constant]; exists {
+	existingAddress, _ := fd.LookupConstant(constantValue)
+	if existingAddress != -1 {
 		return nil
 	}
 
-	fd.Constants[constant] = Constant{
-		virtualDirection: virtualAddress,
-		dataType:         constantType,
+	fd.Constants[virtualAddress] = Constant{
+		Value:    constantValue,
+		DataType: constantType,
 	}
 	return nil
 }
@@ -51,8 +52,10 @@ func (fd *FunctionDirectory) AddConstant(constant string, constantType memory.Da
 //   - If the constant is found in the Constants map, its virtual memory address is returned.
 //   - If the constant is not found, the function returns -1.
 func (fd *FunctionDirectory) LookupConstant(constantValue string) (int, memory.DataType) {
-	if constantInfo, found := fd.Constants[constantValue]; found {
-		return constantInfo.virtualDirection, constantInfo.dataType
+	for virtualAddress, constantInfo := range fd.Constants {
+		if constantValue == constantInfo.Value {
+			return virtualAddress, constantInfo.DataType
+		}
 	}
 
 	return -1, ""
